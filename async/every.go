47b76fe81796d39707f38144a61a7614bc523755
Jihoon Song <jihoonsong@users.noreply.github.com>
2025-09-07 06:31:37 +0900

    Add timers that run at slot ratio

--- async/every.go
+++ async/every.go
@@ -7,23 +7,49 @@ import (
 	"runtime"
 	"time"
 
+	"github.com/OffchainLabs/prysm/v6/time/slots"
 	log "github.com/sirupsen/logrus"
 )
 
 // RunEvery runs the provided command periodically.
 // It runs in a goroutine, and can be cancelled by finishing the supplied context.
-func RunEvery(ctx context.Context, period time.Duration, f func()) {
+func RunEvery(ctx context.Context, interval time.Duration, f func()) {
+	period := func() time.Duration {
+		return interval
+	}
+	runEvery(ctx, period, f)
+}
+
+// RunEverySlotDivision runs the provided command periodically.
+// It runs in a goroutine, and can be cancelled by finishing the supplied context.
+func RunEverySlotDivision(ctx context.Context, genesisTime time.Time, division uint64, f func()) {
+	period := func() time.Duration {
+		return time.Duration(slots.CurrentMillisecondsPerSlot(genesisTime)/division) * time.Millisecond
+	}
+	runEvery(ctx, period, f)
+}
+
+// RunEvery runs the provided command periodically.
+// It runs in a goroutine, and can be cancelled by finishing the supplied context.
+func RunEverySlotMultiple(ctx context.Context, genesisTime time.Time, multiple uint64, f func()) {
+	period := func() time.Duration {
+		slot := slots.CurrentSlot(genesisTime)
+		return slots.SecondsInSlotRange(slot, slot.Add(multiple))
+	}
+	runEvery(ctx, period, f)
+}
+
+// runEvery runs the provided command periodically.
+func runEvery(ctx context.Context, period func() time.Duration, f func()) {
 	funcName := runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
-	ticker := time.NewTicker(period)
 	go func() {
 		for {
 			select {
-			case <-ticker.C:
+			case <-time.After(period()):
 				log.WithField("function", funcName).Trace("Running")
 				f()
 			case <-ctx.Done():
 				log.WithField("function", funcName).Debug("Context is closed, exiting")
-				ticker.Stop()
 				return
 			}
 		}
